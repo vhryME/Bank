d8c1d6ad1827b1aab0a63597a7f54b9f3fa252cf
grits <grits.0123@gmail.com>
2020-06-02 20:55:44 +0300

    Updating Services.

---
+++ src/main/java/com/service/ClientService.java
@@ -0,0 +1,43 @@
+package com.service;
+
+
+import com.model.Client;
+import com.repo.ClientRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class ClientService {
+    @Autowired
+    ClientRepo clientRepo;
+
+    public void createClient(Client client) {
+        if(clientRepo != null)
+            clientRepo.save(client);
+    }
+
+
+    public void deleteUser(Client client) {
+        if(clientRepo != null)
+            clientRepo.delete(client);
+    }
+
+
+    public Client getClientById(long id) {
+        if(clientRepo != null)
+            return clientRepo.getOne(id);
+
+        return null;
+    }
+
+
+    public List<Client> getAllClients() {
+        if(clientRepo != null)
+            return clientRepo.findAll();
+
+        return null;
+    }
+}
---
+++ src/main/java/com/service/UserService.java
@@ -0,0 +1,43 @@
+package com.service;
+
+
+import com.model.User;
+import com.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepo userRepo;
+
+    public void createUser(User user) {
+        if(userRepo != null)
+            userRepo.save(user);
+    }
+
+
+    public void deleteUser(User user) {
+        if(userRepo != null)
+            userRepo.delete(user);
+    }
+
+
+    public User getUserByName(String name) {
+        if(userRepo != null)
+            return userRepo.getByName(name);
+
+        return null;
+    }
+
+
+    public List<User> getAllUsers() {
+        if(userRepo != null)
+            return userRepo.findAll();
+
+        return null;
+    }
+}
